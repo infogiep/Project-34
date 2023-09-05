e3a1575609e4aeb3edb28d60b0cfcd6417f29c3a
Alex <122611217+infogiep@users.noreply.github.com>
2023-09-05 07:55:17 +0300

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+template<typename T> 
+concept ComplexConcept = !std::has_virtual_destructor<T>::value && requires (T t)
+{
+    {t.hash()}->std::convertible_to<long>;
+    {t.toString()}->std::same_as<std::string>;
+};
+
+struct y
+{
+    int z = 0;
+    std::string toString() { return std::to_string(z); }
+    int hash() { return 1; };
+};
+
+void print(ComplexConcept  auto& t)
+{
+    std::cout << t.toString() << std::endl;
+}
+
+int main()
+{
+    y x{ 20 };
+    print(x);
+}
